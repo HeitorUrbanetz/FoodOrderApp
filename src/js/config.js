e0d6ca72aa99e638017d99225b0cc39fa05e6d20
Heitor Urbanetz <jhty123453@gmail.com>
2022-04-19 15:00:25 -0300

    first commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'ecc69e06-b38d-4241-900a-8453a90f2c12';
+export const MODAL_CLOSE_SEC = 2.5;
